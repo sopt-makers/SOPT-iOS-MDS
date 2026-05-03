6985c92bbf0e277521319ce6d32e2ac413acb0ae
yungu0010 <dbstj16@sookmyung.ac.kr>
2026-05-03 18:05:21 +0900

    [Add] #4 - FontLoader 추가

---
+++ MDS/Sources/MDS/Foundation/Base/FontLoader.swift
@@ -0,0 +1,35 @@
+//
+//  FontLoader.swift
+//  MDS
+//
+//  Created by 강윤서 on 5/3/26.
+//
+
+import CoreText
+import Foundation
+
+internal enum FontLoader {
+    private static var isRegistered = false
+
+    static func registerIfNeeded() {
+        guard !isRegistered else { return }
+        isRegistered = true
+
+        let fontNames = [
+            "Pretendard-Thin",
+            "Pretendard-ExtraLight",
+            "Pretendard-Light",
+            "Pretendard-Regular",
+            "Pretendard-Medium",
+            "Pretendard-SemiBold",
+            "Pretendard-Bold",
+            "Pretendard-ExtraBold",
+            "Pretendard-Black",
+        ]
+
+        fontNames.forEach { name in
+            guard let url = Bundle.module.url(forResource: name, withExtension: "otf") else { return }
+            CTFontManagerRegisterFontsForURL(url as CFURL, .process, nil)
+        }
+    }
+}
