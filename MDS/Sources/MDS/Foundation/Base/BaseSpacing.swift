b4a802cc186d2c45d2e4f0628fe7d78284184178
yungu0010 <65678579+yungu0010@users.noreply.github.com>
2026-05-06 12:57:29 +0000

    [Style Dictionary] Update generated Swift token files

--- MDS/Sources/MDS/Foundation/Base/BaseSpacing.swift
+++ MDS/Sources/MDS/Foundation/Base/BaseSpacing.swift
@@ -11,28 +11,28 @@ import UIKit
 
 internal enum BaseSpacing {
 
-    // MARK: Size
-    enum Size {
-        static let size0: CGFloat = 0
-        static let size2: CGFloat = 2
-        static let size4: CGFloat = 4
-        static let size6: CGFloat = 6
-        static let size8: CGFloat = 8
-        static let size10: CGFloat = 10
-        static let size12: CGFloat = 12
-        static let size14: CGFloat = 14
-        static let size16: CGFloat = 16
-        static let size20: CGFloat = 20
-        static let size24: CGFloat = 24
-        static let size28: CGFloat = 28
-        static let size32: CGFloat = 32
-        static let size36: CGFloat = 36
-        static let size40: CGFloat = 40
-        static let size48: CGFloat = 48
-        static let size64: CGFloat = 64
-        static let size72: CGFloat = 72
-        static let size80: CGFloat = 80
-        static let size120: CGFloat = 120
-        static let size160: CGFloat = 160
+    // MARK: Base
+    enum Base {
+        static let s0: CGFloat = 0
+        static let s2: CGFloat = 2
+        static let s4: CGFloat = 4
+        static let s6: CGFloat = 6
+        static let s8: CGFloat = 8
+        static let s10: CGFloat = 10
+        static let s12: CGFloat = 12
+        static let s14: CGFloat = 14
+        static let s16: CGFloat = 16
+        static let s20: CGFloat = 20
+        static let s24: CGFloat = 24
+        static let s28: CGFloat = 28
+        static let s32: CGFloat = 32
+        static let s36: CGFloat = 36
+        static let s40: CGFloat = 40
+        static let s48: CGFloat = 48
+        static let s64: CGFloat = 64
+        static let s72: CGFloat = 72
+        static let s80: CGFloat = 80
+        static let s120: CGFloat = 120
+        static let s160: CGFloat = 160
     }
 }
