7c350cb913776f5bc6deb9a411a76ff5fcb952ef
yungu0010 <dbstj16@sookmyung.ac.kr>
2026-05-08 19:11:12 +0900

    [Chore] #42 - MDSFont의 @unchecked Sendable 사용 이유 주석 추가

--- MDS/Sources/MDS/Foundation/MDSFont.swift
+++ MDS/Sources/MDS/Foundation/MDSFont.swift
@@ -5,6 +5,8 @@
 
 import UIKit
 
+/// `@unchecked` Sendable conformance: UIFont는 불변 객체로서 Apple 문서에서 명시적으로
+/// 다중 스레드 안전성을 보장하며, 모든 저장 프로퍼티(UIFont, CGFloat)는 불변입니다.
 public struct MDSFont: @unchecked Sendable {
     public let font: UIFont
     public let lineHeight: CGFloat
