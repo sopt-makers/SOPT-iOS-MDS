0d53b6d492c929a770a1d345b5039f4925345828
yungu0010 <dbstj16@sookmyung.ac.kr>
2026-05-18 17:32:15 +0900

    [Feat] #69 - MDSTag Enum 타입 정의
    Size, Shape, Variant, Style enum을 MDSTag의 extension으로 분리하여 정의합니다.
    컴포넌트 본체와 타입 정의를 파일 단위로 분리해 가독성을 높입니다.

---
+++ MDS/Sources/Components/MDSTagType.swift
@@ -0,0 +1,25 @@
+//
+//  MDSTagType.swift
+//  MDS
+//
+//  Created by yungu0010 on 5/18/26.
+//
+
+public extension MDSTag {
+
+    enum Size {
+        case small, medium, large
+    }
+
+    enum Shape {
+        case rect, pill
+    }
+
+    enum Variant {
+        case `default`, primary, secondary
+    }
+
+    enum Style {
+        case solid, subtle
+    }
+}
