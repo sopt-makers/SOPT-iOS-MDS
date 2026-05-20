a972ee07715232c41458d67b343bb5ec47814ce9
JURI <80569323+juri123123@users.noreply.github.com>
2026-05-21 00:09:52 +0900

    [Feat] #77 콜아웃 컴포넌트 구현

---
+++ MDS/Sources/Components/Callout/MDSCalloutType.swift
@@ -0,0 +1,15 @@
+//
+//  MDSCalloutType.swift
+//  MDS
+//
+//  Created by 최주리 on 5/20/26.
+//
+
+import UIKit
+
+extension MDSCallout {
+    public enum Style: CaseIterable {
+        case danger
+        case information
+    }
+}
