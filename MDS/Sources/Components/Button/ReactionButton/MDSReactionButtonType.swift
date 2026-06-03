c2ea5e02bfd88828a416184a260618568ea5766a
JURI <80569323+juri123123@users.noreply.github.com>
2026-06-04 01:22:56 +0900

    [Feat] #99 - Reaction Button 구현

---
+++ MDS/Sources/Components/Button/ReactionButton/MDSReactionButtonType.swift
@@ -0,0 +1,20 @@
+//
+//  MDSReactionButtonType.swift
+//  MDS
+//
+//  Created by 최주리 on 6/4/26.
+//
+
+public extension MDSReactionButton {
+    enum Variant {
+        case `default`
+        case disabled
+    }
+
+    enum Size {
+        case xsmall
+        case small
+        case medium
+        case large
+    }
+}
