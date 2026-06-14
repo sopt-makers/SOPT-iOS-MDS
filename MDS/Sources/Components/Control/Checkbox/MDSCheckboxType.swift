52d9765c39cd445811796cf633694ae871f780a1
JURI <80569323+juri123123@users.noreply.github.com>
2026-06-14 21:47:06 +0900

    [Feat] #105 - checkbox 구현

---
+++ MDS/Sources/Components/Control/Checkbox/MDSCheckboxType.swift
@@ -0,0 +1,15 @@
+//
+//  MDSCheckboxType.swift
+//  MDS
+//
+//  Created by 최주리 on 6/6/26.
+//
+
+import Foundation
+
+public extension MDSCheckbox {
+    enum Size {
+        case small
+        case large
+    }
+}
